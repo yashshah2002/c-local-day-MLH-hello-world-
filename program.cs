37d2c4ed7dd70ba37a75188832348a426d5024f9
yash shah <68466768+yashshah2002@users.noreply.github.com>
2021-01-11 20:57:06 +0530

    c# hello world

---
+++ program.cs
@@ -0,0 +1,16 @@
+using system;
+
+namespace MyApp 
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            console.WriteLine("Hello world ");
+            console.WriteLine("MLH hack day ");
+            var name = "Yash";
+            console.WriteLine($"I am (name)");
+        }
+    }
+}
